51072af5809e1b525dfc871da8f232b0dfc25ff2
dalmita <dalmavelasquez1@gmail.com>
2023-08-23 19:37:02 -0300

    vista del home , creacion de productos y cambio de extencion a ejs

--- app.js
+++ app.js
@@ -2,10 +2,23 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const PORT = 3001;
+//requerir method overside para hacer formulaario de edicion de producto
+const methoOverride=require('method-override');
+//confijuracion de ejs
+
+app.set("view engine", "ejs");
+
+//ar imagen =document.createElement('img');
+//img.src= "images/cat-parlante.png";
+//document.getElementById("idDelElemento".appendChild(imagen))
+
+
+
+
+
 
 app.use(express.static(path.join(__dirname, "/public")))
 
-app.get("/", (req,res) => res.sendFile(path.join(__dirname, "views/home.html")))
 
 app.get("/carrito", (req,res) => res.sendFile(path.join(__dirname, "views/carritoDeCompras.html")))
 
@@ -24,3 +37,5 @@ app.post("/register", (req,res) =>{
 app.post("/login", (req,res) =>{
     res.redirect("/")
 })
+//Indico que voy a usa, el metodo sirve para poder usar el metodo POST para el formulario de edicion de producto
+app.use(methoOverride('_method'))
