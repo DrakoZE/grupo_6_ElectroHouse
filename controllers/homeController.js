3c876e8aa87fe2736bd83da14066ce5355be3a2d
dalmita <dalmavelasquez1@gmail.com>
2023-08-23 20:17:06 -0300

    correcion de home , seccion y muestra de productos

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,6 +1,24 @@
 const homeController ={
     getHomePage: (req, res) => {
-        res.render("products/home");
+      let productos = [
+        {
+          id: 1,
+          titulo : "titulo 1"
+        },
+        {
+          id: 2,
+          titulo : "titulo 2"
+        },
+        {
+          id: 4,
+          titulo : "titulo 4"
+        },
+
+
+
+
+      ]
+        res.render("products/home", {productos:productos});
       },
     };
 module.exports = homeController;
