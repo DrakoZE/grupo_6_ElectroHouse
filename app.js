b090d5dbbb9bc1ebd1c65e1170c5dc33cba3ff7c
DrakoZe <maxizenon625@gmail.com>
2023-09-08 23:04:10 -0300

    Creacion de la vista detail

--- app.js
+++ app.js
@@ -32,5 +32,9 @@ app.use("/", registerRouter);
 app.use("/", loginRouter);
 app.use("/", carritoRouter);
 
+app.get("/detail", (req,res)=> {
+    res.render("products/detailProduct")
+})
+
 app.listen(PORT, () => console.log("El servidor esta corriendo en el puerto: " + PORT));
 
