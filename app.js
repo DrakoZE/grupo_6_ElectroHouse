ae63994531d372d391c08bdc6906008faa06f0fb
rogallardo06 <rosabelu18@gmail.com>
2023-09-11 21:18:37 -0300

    crud-r

--- app.js
+++ app.js
@@ -32,5 +32,6 @@ app.use("/", registerRouter);
 app.use("/", loginRouter);
 app.use("/", carritoRouter);
 
+
 app.listen(PORT, () => console.log("El servidor esta corriendo en el puerto: " + PORT));
 
--- routers/productos.js
+++ routers/productos.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/productoController");
 
-router.get("/producto", controller.getProductoPage);
+router.get("/producto", controller.getProductsPage);
+
+
 
 module.exports = router
 
