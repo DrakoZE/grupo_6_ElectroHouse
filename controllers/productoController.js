e1c58d1fad21cf253af860801df093fa71a49392
Taguminn <aledanielduran@gmail.com>
2023-09-24 03:17:52 -0300

    view update for the products page

--- controllers/productoController.js
+++ controllers/productoController.js
@@ -6,21 +6,30 @@ let productos = [
     titulo: "Parlante CAT",
     precio: "$6.770",
     descuento: "40% off",
-    imagen: "images/cat-parlante.png"
+    imagen: "images/cat-parlante.png",
+    categoria: "Audio",
+    color: "#1a7063",
+    color1: "#1fe9cb"
   },
   {
     id: 2,
-    titulo: "Iphone 14 Promax",
+    titulo: "Iphone 14",
     precio: "$230.000",
     descuento: "20% off",
-    imagen: "images/iphone-14-promax.png"
+    imagen: "images/iphone-14-promax.png",
+    categoria: "Smartphone",
+    color: "#512224",
+    color1: "#ff2e37"
   },
   {
     id: 3,
-    titulo: "Control de XBOX",
+    titulo: "Control XBOX",
     precio: "$23.200",
     descuento: "5% off",
-    imagen: "images/xbox-control.png"
+    imagen: "images/xbox-control.png",
+    categoria: "Entretenimiento",
+    color: "#4f3f1f",
+    color1: "#ffec08"
   },
 ]
 
@@ -87,14 +96,14 @@ const productoController = {
 
   getProductsPage: (req, res) => {
     const products = productos;
-    res.render(`products/products`, {productos: products});
+    res.render(`products/products`, {productos: productos});
   },
 
   getDetailpage: (req, res) => {
     const products = productos;
     const product = products.find(productA => productA.id == req.params.id);
 
-    res.render("products/detailProduct", { product })
+    res.render("products/detailProduct", {productos: productos})
   },
 
   getCarritoPage: (req, res) => {
