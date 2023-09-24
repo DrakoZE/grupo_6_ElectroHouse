54d5855beef453a2c27c6ba970ccb20c48d08bb3
Taguminn <aledanielduran@gmail.com>
2023-09-24 01:09:04 -0300

    Rework of the home page

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -6,21 +6,30 @@ const homeController = {
         titulo: "Parlante CAT",
         precio: "$6.770",
         descuento: "40% off",
-        imagen: "images/cat-parlante.png"
+        imagen: "images/cat-parlante.png",
+        categoria: "Audio",
+        color: "#1a7063",
+        color1: "#1fe9cb"
       },
       {
         id: 2,
-        titulo: "Iphone 14 Promax",
+        titulo: "Iphone 14",
         precio: "$230.000",
         descuento: "20% off",
-        imagen: "images/iphone-14-promax.png"
+        imagen: "images/iphone-14-promax.png",
+        categoria: "Smartphone",
+        color: "#512224",
+        color1: "#ff2e37"
       },
       {
         id: 3,
-        titulo: "Control de XBOX",
+        titulo: "Control XBOX",
         precio: "$23.200",
         descuento: "5% off",
-        imagen: "images/xbox-control.png"
+        imagen: "images/xbox-control.png",
+        categoria: "Entretenimiento",
+        color: "#4f3f1f",
+        color1: "#ffec08"
       },
 
 
